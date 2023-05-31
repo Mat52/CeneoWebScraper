78fdc018f664d9f6da7313c0043f728fd3140c34
Mat52 <mati200205@hotmail.com>
2023-05-31 16:16:33 +0200

    templates 31.05.2023 MŁ

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,30 @@
 from app import app
-
+from flask import render_template
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello world"
+    return render_template("main.html")
+
+@app.route("/extraction")
+def extraction():
+    return render_template("extraction.html")
+
+@app.route("/productlist")
+def productlist():
+    return render_template("productlist.html")
+
+@app.route("/product")
+def product():
+    return render_template("product.html")
+
+@app.route("/author")
+def author():
+    return render_template("author.html")
+
+@app.route("/chart")
+def chart():
+    return render_template("chart.html")
+
 @app.route("/name/", defaults={"name": "Anonim"})
 @app.route("/name/<name>")
 def name(name):
